44b47561dc809be9df61ceff157dc6a42d69072e
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2007-12-06 01:16:01 +0000

    Updating bundled author tests
    git-svn-id: http://svn.ali.as/cpan/trunk/File-HomeDir@2457 88f4d9cd-8a04-0410-9d60-8f63309c3137

--- lib/File/HomeDir.pm
+++ lib/File/HomeDir.pm
@@ -10,7 +10,7 @@ use File::Spec ();
 # Globals
 use vars qw{$VERSION @ISA @EXPORT @EXPORT_OK $IMPLEMENTED_BY};
 BEGIN {
-	$VERSION = '0.66';
+	$VERSION = '0.67';
 
 	# Inherit manually
 	require Exporter;
