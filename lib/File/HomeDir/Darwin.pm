6da134d214f726116db5046d89c6c875bb49d4b4
wyant@cpan.org <wyant@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-10-31 19:23:55 +0000

    Ensure users_home() returns undef if SUPER::users_home() does. Have _to_user() return undef if users_home() does, and escape the 'Q' to quote $my_home in the regular expression that transmogrifies the path.
    git-svn-id: http://svn.ali.as/cpan/trunk/File-HomeDir@4516 88f4d9cd-8a04-0410-9d60-8f63309c3137

--- lib/File/HomeDir/Darwin.pm
+++ lib/File/HomeDir/Darwin.pm
@@ -11,7 +11,7 @@ use File::HomeDir::Unix ();
 
 use vars qw{$VERSION @ISA};
 BEGIN {
-	$VERSION = '0.82';
+	$VERSION = '0.82_01';
 	@ISA     = 'File::HomeDir::Unix';
 }
 
@@ -112,7 +112,7 @@ sub _find_folder {
 sub users_home {
 	my $class = shift;
 	my $home  = $class->SUPER::users_home(@_);
-	return Cwd::abs_path($home);
+	return defined $home ? Cwd::abs_path($home) : undef;
 }
 
 # in theory this can be done, but for now, let's cheat, since the
@@ -141,7 +141,8 @@ sub _to_user {
 	my ($class, $path, $name) = @_;
 	my $my_home    = $class->my_home;
 	my $users_home = $class->users_home($name);
-	$path =~ s/^Q$my_home/$users_home/;
+	defined $users_home or return undef;
+	$path =~ s/^\Q$my_home/$users_home/;
 	return $path;
 }
 
