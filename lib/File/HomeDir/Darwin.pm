97a2805c524decba18621fc73877891481909c26
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-10-03 13:50:44 +0000

    Second commit moving towards a new HomeDir dev release
    git-svn-id: http://svn.ali.as/cpan/trunk/File-HomeDir@9497 88f4d9cd-8a04-0410-9d60-8f63309c3137

--- lib/File/HomeDir/Darwin.pm
+++ lib/File/HomeDir/Darwin.pm
@@ -1,7 +1,4 @@
-package File::HomeDir::Darwin;
-
-# Basic implementation for the Dawin family of operating systems.
-# This includes (most prominently) Mac OS X.
+package File::HomeDir::DarwinPerl;
 
 use 5.00503;
 use strict;
@@ -15,143 +12,104 @@ BEGIN {
 	@ISA     = 'File::HomeDir::Unix';
 }
 
-# Load early if in a forking environment and we have
-# prefork, or at run-time if not.
-SCOPE: {
-	local $@;
-	eval "use prefork 'Mac::Files'";
-}
-
-
-
-
-
 #####################################################################
 # Current User Methods
 
 sub my_home {
-	my ($class) = @_;
+    my ($class) = @_;
 
-	# A lot of unix people and unix-derived tools rely on
-	# the ability to overload HOME. We will support it too
-	# so that they can replace raw HOME calls with File::HomeDir.
-	if ( exists $ENV{HOME} and defined $ENV{HOME} ) {
-		return $ENV{HOME};
-	}
-	
-  require Mac::Files;
-	$class->_find_folder(
-		Mac::Files::kCurrentUserFolderType(),
-		);
+    if ( exists $ENV{HOME} and defined $ENV{HOME} ) {
+        return $ENV{HOME};
+    }
+
+    my $home = (getpwuid($<))[7];
+    return $home if $home && -d $home;
+
+    return undef;
+}
+
+sub _my_home {
+    my($class, $path) = @_;
+    my $home = $class->my_home;
+    return undef unless defined $home;
+
+    my $folder = "$home/$path";
+    unless ( -d $folder ) {
+        # Make sure that symlinks resolve to directories.
+        return unless -l $folder;
+        my $dir = readlink $folder or return;
+        return unless -d $dir;
+    }
+
+    return Cwd::abs_path($folder);
 }
 
 sub my_desktop {
-	my ($class) = @_;
-	require Mac::Files;
-	$class->_find_folder(
-		Mac::Files::kDesktopFolderType(),
-		);
+    my ($class) = @_;
+    $class->_my_home('Desktop');
 }
 
 sub my_documents {
-	my ($class) = @_;
-	require Mac::Files;
-	$class->_find_folder(
-		Mac::Files::kDocumentsFolderType(),
-		);
+    my ($class) = @_;
+    $class->_my_home('Documents');
 }
 
 sub my_data {
-	my ($class) = @_;
-	require Mac::Files;
-	$class->_find_folder(
-		Mac::Files::kApplicationSupportFolderType(),
-		);
+    my ($class) = @_;
+    $class->_my_home('Library/Application Support');
 }
 
 sub my_music {
-	my ($class) = @_;
-	require Mac::Files;
-	$class->_find_folder(
-		Mac::Files::kMusicDocumentsFolderType(),
-		);
+    my ($class) = @_;
+    $class->_my_home('Music');
 }
 
 sub my_pictures {
-	my ($class) = @_;
-	require Mac::Files;
-	$class->_find_folder(
-		Mac::Files::kPictureDocumentsFolderType(),
-		);
+    my ($class) = @_;
+    $class->_my_home('Pictures');
 }
 
 sub my_videos {
-	my ($class) = @_;
-	require Mac::Files;
-	$class->_find_folder(
-		Mac::Files::kMovieDocumentsFolderType(),
-		);
+    my ($class) = @_;
+    $class->_my_home('Movies');
 }
-
-sub _find_folder {
-	my ($class, $name) = @_;
-	require Mac::Files;
-	my $folder = Mac::Files::FindFolder(
-		Mac::Files::kUserDomain(),
-		$name,
-		);
-	return unless defined $folder;
-	unless ( -d $folder ) {
-		# Make sure that symlinks resolve to directories.
-		return unless -l $folder;
-		my $dir = readlink $folder or return;
-		return unless -d $dir;
-	}
-	return Cwd::abs_path($folder);
-}
-
-
-
-
 
 #####################################################################
 # Arbitrary User Methods
 
 sub users_home {
-	my $class = shift;
-	my $home  = $class->SUPER::users_home(@_);
-	return defined $home ? Cwd::abs_path($home) : undef;
+    my $class = shift;
+    my $home  = $class->SUPER::users_home(@_);
+    return defined $home ? Cwd::abs_path($home) : undef;
 }
 
-# in theory this can be done, but for now, let's cheat, since the
-# rest is Hard
 sub users_desktop {
-	my ($class, $name) = @_;
-	return undef if $name eq 'root';
-	$class->_to_user( $class->my_desktop, $name );
+    my ($class, $name) = @_;
+    return undef if $name eq 'root';
+    $class->_to_user( $class->my_desktop, $name );
 }
 
 sub users_documents {
-	my ($class, $name) = @_;
-	return undef if $name eq 'root';
-	$class->_to_user( $class->my_documents, $name );
+    my ($class, $name) = @_;
+    return undef if $name eq 'root';
+    $class->_to_user( $class->my_documents, $name );
 }
 
 sub users_data {
-	my ($class, $name) = @_;
-	$class->_to_user( $class->my_data, $name )
-		|| $class->users_home($name);
+    my ($class, $name) = @_;
+    $class->_to_user( $class->my_data, $name )
+        || $class->users_home($name);
 }
 
 # cheap hack ... not entirely reliable, perhaps, but ... c'est la vie, since
 # there's really no other good way to do it at this time, that i know of -- pudge
 sub _to_user {
-	my ($class, $path, $name) = @_;
-	my $my_home    = $class->my_home;
-	my $users_home = $class->users_home($name);
-	defined $users_home or return undef;
-	$path =~ s/^\Q$my_home/$users_home/;
-	return $path;
+    my ($class, $path, $name) = @_;
+    my $my_home    = $class->my_home;
+    my $users_home = $class->users_home($name);
+    defined $users_home or return undef;
+    $path =~ s/^\Q$my_home/$users_home/;
+    return $path;
 }
 
 1;
@@ -160,22 +118,19 @@ sub _to_user {
 
 =head1 NAME
 
-File::HomeDir::Darwin - find your home and other directories, on Darwin (OS X)
+File::HomeDir::DarwinPerl - find your home and other directories, on Darwin (OS X) without Carbon/Cocoa
 
 =head1 DESCRIPTION
 
-This module provides Darwin-specific implementations for determining
-common user directories.  In normal usage this module will always be
-used via L<File::HomeDir>.
-
-Note -- since this module requires Mac::Carbon and Mac::Carbon does
-not work with 64-bit perls, on such systems, File::HomeDir will try
-File::HomeDir::DarwinCocoa and then fall back to File::HomeDir::DarwinPerl.
+This module provides Mac OS X specific file path for determining
+common user directories in pure perl, by just using C<$ENV{HOME}>
+without Carbon nor Cocoa API calls. In normal usage this module will
+always be used via L<File::HomeDir>.
 
 =head1 SYNOPSIS
 
   use File::HomeDir;
-
+  
   # Find directories for the current user
   $home    = File::HomeDir->my_home;      # /Users/mylogin
   $desktop = File::HomeDir->my_desktop;   # /Users/mylogin/Desktop
@@ -184,13 +139,5 @@ File::HomeDir::DarwinCocoa and then fall back to File::HomeDir::DarwinPerl.
   $pics    = File::HomeDir->my_pictures;  # /Users/mylogin/Pictures
   $videos  = File::HomeDir->my_videos;    # /Users/mylogin/Movies
   $data    = File::HomeDir->my_data;      # /Users/mylogin/Library/Application Support
-
-=head1 TODO
-
-=over 4
-
-=item * Test with Mac OS (versions 7, 8, 9)
-
-=item * Some better way for users_* ?
 
 =back
